c1cb42448dc0434c20c3972add09e2050b1e6efe
ZhongLi <swmfizl@163.com>
2020-01-12 14:51:42 +0800

    v1.0.0

---
+++ src/main/java/com/websocket/websocket/WebSocket.java
@@ -0,0 +1,116 @@
+package com.websocket.websocket;
+
+import java.io.IOException;
+import java.util.concurrent.ConcurrentHashMap;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * WebSocket
+ * 
+ * @author zhongli
+ * @date 2020-01-12
+ * @version 1.0.0
+ */
+@ServerEndpoint("/WebSocket/{id}")
+@RestController
+public class WebSocket {
+
+	// 存储会话
+	private static ConcurrentHashMap<String, WebSocket> webSocket = new ConcurrentHashMap<String, WebSocket>();
+
+	private String id;
+	private Session session;
+
+	/**
+	 * 接入连接回调
+	 * 
+	 * @param session 会话对象
+	 * @param id      会话ID
+	 * @throws Exception 异常
+	 */
+	@OnOpen
+	public void onOpen(Session session, @PathParam("id") String id) throws Exception {
+		this.id = id;
+		this.session = session;
+		webSocket.put(id, this);
+		System.out.println(id + "接入连接");
+	}
+
+	/**
+	 * 关闭连接回调
+	 */
+	@OnClose
+	public void onClose() {
+		webSocket.remove(this.id);
+		System.out.println(this.id + "关闭连接");
+	}
+
+	/**
+	 * 收到客户端发来消息回调
+	 * 
+	 * @param message
+	 * @param session
+	 */
+	@OnMessage
+	public void onMessage(String message, Session session) {
+		System.out.println(this.id + "发来消息：" + message);
+	}
+
+	/**
+	 * 会话出现错误回调
+	 * 
+	 * @param session 会话对象
+	 * @param error   错误信息
+	 */
+	@OnError
+	public void onError(Session session, Throwable error) {
+
+	}
+
+	/**
+	 * 发送消息给客户端
+	 * 
+	 * @param message 消息
+	 * @throws IOException 异常
+	 */
+	public void sendMessage(String message) throws IOException {
+		this.session.getBasicRemote().sendText(message);
+	}
+
+	/**
+	 * 给指定的会话发送消息
+	 * 
+	 * @param id      会话ID
+	 * @param message 消息
+	 * @throws IOException 异常
+	 */
+	public void sendMessageToId(String id, String message) throws IOException {
+		webSocket.get(id).sendMessage(message);
+	}
+
+	/**
+	 * 群发消息
+	 * 
+	 * @param message 消息
+	 * @throws IOException 异常
+	 */
+	public void sendMessageToAll(String message) throws IOException {
+		for (String key : webSocket.keySet()) {
+			try {
+				webSocket.get(key).sendMessage(message);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
---
+++ src/main/java/com/websocket/websocket/WebSocketConfig.java
@@ -0,0 +1,20 @@
+package com.websocket.websocket;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * WebSocketConfig
+ * 
+ * @author zhongli
+ * @date 2020-01-12
+ * @version 1.0.0
+ */
+@Configuration
+public class WebSocketConfig {
+	@Bean
+	public ServerEndpointExporter serverEndpointExporter() {
+		return new ServerEndpointExporter();
+	}
+}
